a94cdd7c46b1cfd82c407c7396d55126cf422910
ViktorTester <viktor345@inbox.lv>
2024-10-20 19:43:52 +0300

    Frames.java updated

--- src/test/java/day30_Frames/Frames.java
+++ src/test/java/day30_Frames/Frames.java
@@ -47,5 +47,32 @@ public class Frames {
         // Нахождение локатора инпут-поля внутри фрейма и передача в него текста
         driver.findElement(By.xpath("//input [@name='mytext2']")).sendKeys("selenium");
 
+
+// Frame 3
+        // Во фрейме 3 находится еще один фрейм с формой для заполнения,
+        // и для взаимодействия с ним сперва нужно перейти во фрейм 3, а
+        // потом уже в сам фрейм с формой
+
+        // Нахождение локатора фрейма и переключение драйвера на него
+        WebElement frame3 = driver.findElement(By.xpath("//frame[@src='frame_3.html']"));
+        driver.switchTo().frame(frame3);
+
+        // Находясь во фрейме 3, передаем в инпут-поле значение
+        driver.findElement(By.xpath("//input [@name='mytext3']")).sendKeys("Java");
+
+
+    // Вложенный фрейм
+
+        // Так как нужно переместиться во фрейм, который вложен во фрейм 3, то это
+        // можно сделать напрямую, без возвращения на основную страницу
+        // Переключение с помощью индекса фрейма
+        driver.switchTo().frame(0);
+
+        // Клик по одному из чекбоксов вложенного фрейма
+        driver.findElement(By.xpath("//div[@id='18']//div[@class='AB7Lab Id5V1']")).click();
+
+        // Возврат на основную страницу
+        driver.switchTo().defaultContent();
+
     }
 }
